57032b537685eceae4a72806783599b77716f253
Shumpei Shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2022-02-21 15:05:42 +0900

    add sample doctest usage and CMakeLists.txt

---
+++ include/pcas/pcas.hpp
@@ -0,0 +1,11 @@
+#include "doctest.h"
+
+static inline int factorial(int number) { return number <= 1 ? 1 : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function") {
+  CHECK(factorial(0) == 1);
+  CHECK(factorial(1) == 1);
+  CHECK(factorial(2) == 2);
+  CHECK(factorial(3) == 6);
+  CHECK(factorial(10) == 3628800);
+}
